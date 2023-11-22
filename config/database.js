03ca8684c713295c62af51f1ac77ee82c9f98ab3
sarthak <sarthaksri044@gmail.com>
2023-11-22 10:37:21 +0530

    error

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser:true,
+         useUnifiedTopology: true, 
+     })
     .then(() => {console.log("db connected successfully")})
     .catch( (err) => {
         console.log("db connection issue");
